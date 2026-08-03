70cfc4bc8a781ab27ed4a95c96ca0d62f9f8ae09
sereno766 <acalusereno@hotmail.com>
2026-08-03 17:26:31 -0300

    update parse for only confing.txt

--- program/a_maze_ing/__main__.py
+++ program/a_maze_ing/__main__.py
@@ -4,24 +4,23 @@ import argparse
 
 def main() -> int:
     parser = argparse.ArgumentParser()
-    parser.add_argument("-c", "--config", nargs="?", type=config_file,
+    parser.add_argument("config", type=config_file,
                         help="Maze configuration file")
 
     args = parser.parse_args()
 
-    if args.config is not None:
-        try:
-            settings = runner(fpath=args.config)
-            print(f"settings: {settings}")
-        except Exception as e:
-            print(f"{RED}Error: {e}{DEFAULT}")
-            return 1
+    try:
+        settings = runner(fpath=args.config)
+        print(f"settings: {settings}")
+    except Exception as e:
+        print(f"{RED}Error: {e}{DEFAULT}")
+        return 1
     return 0
 
 
 if __name__ == "__main__":
-    # main()
+    main()
     # print(gen_chars(50))
     # print(gen_nbr(3))
-    print(agc())
+    # print(agc())
     # gen_seed()
